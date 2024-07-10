a5385b1e9fc311cd16be9527aae8962aefdbfb1a
Dhanwanth kumar M <100747406+dhanwanthkumar@users.noreply.github.com>
2024-07-10 11:28:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function makePayment() {
     const upiId = 'dhanwanthmohan@oksbi';
     const amount = 10; // Amount in dollars
-    const upiPaymentUrl = "https://upilinks.in/payment-link/upi4519273741";
+    const upiPaymentUrl = "https://upilinks.in/payment-link/upi451927374";
 
     // Redirect to UPI payment URL
     window.location.href = upiPaymentUrl;
